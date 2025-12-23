bbc9e11ba960a4be8f772e8e4404ef46d4b20b7e
samandarjumayev <samandarjumayev005@gmail.com>
2025-12-23 23:44:09 +0500

    update

--- src/pages/public/StartHomePage.jsx
+++ src/pages/public/StartHomePage.jsx
@@ -3,6 +3,7 @@ import StartHeader from "../../shared/components/StartHeader";
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import cloudPNG from '../../../public/cloud.png'
+import video from '../../../public/videos/Mirjalol Nematov - Malak.mp4'
 
 export default function StartHomePage(){
     const [viewPNG, setViewPNG] = useState(false);
@@ -22,32 +23,39 @@ export default function StartHomePage(){
     return <div className="w-full min-h-screen bg-[linear-gradient(180deg,#010336_0%,#0a0f2c_40%,#020617_100%)] text-white">
         <StartHeader />
         
-        <div className={`relative px-[32px] mx-auto w-full h-[calc(100vh-72px)] flex items-center justify-center flex-col gap-3 selection:bg-white/0 cursor-context-menu`}>
-            <NavLink className={`bg-[linear-gradient(90deg,#0A0B5F_0%,#242B8E_50%,#453F9A_100%)] flex gap-5 items-center py-4 px-7 border border-[#30363d] rounded-full mb-4 transition-all duration-150 active:duration-50 active:scale-97`}>
-                <img 
-                    src="https://cdn-icons-png.flaticon.com/128/3196/3196017.png" 
-                    alt="buqalamun, hamilyon" 
-                    className="h-[45px]" 
-                />
-                <p className="text-[20px] tracking-[0.5px] font-semibold">Create your team on TeamFlow</p>
-                <ChevronRight size={19} />
-            </NavLink>
+        <div className={`${scrollY > 0 ? 'z-[0]' : ''} sticky top-[72px] left-0 px-[32px] mx-auto w-full h-[calc(100vh-72px)] selection:bg-white/0 cursor-context-menu transition-all duration-800`}>
 
-            <h2 className={`text-[64px] font-semibold text-center leading-[64px]`}>Build real projects with your <br /> team.</h2>
-            <p className="text-[20px] text-[#cacaca] text-center my-4 leading-[27px]">Collaborate with other developers, manage tasks, and turn <br /> ideas into real working products — all in one place.</p>
-
-            <div className="flex z-5 items-center gap-4 mb-14">
-
-                <div className={`bg-white flex items-center gap-1 p-1 rounded-[8px] h-[55px]`}>
-                    <input type="text" placeholder="Enter your email" className="placeholder:text-[#6d6d6d] text-black text-[16px] font-semibold outline-none h-full px-5" />
-                    <button className="bg-[#1a7f37] text-white h-full rounded-[6px] px-6 font-semibold text-[18px] cursor-pointer transition-all duration-150 active:duration-50 active:scale-95">Sign up for TeamFlow</button>
-                </div>
-
-                <NavLink to={'/'} className={`border-2 h-[55px] px-15 rounded-[8px] flex items-center justify-center text-[18px] font-semibold cursor-pointer transition-all duration-150 active:duration-50 active:scale-95`}>
-                    Login
+            <div className={`
+                w-full h-full flex items-center justify-center flex-col gap-3 transition-all duration-700
+                ${scrollY > 100 ? `${scrollY > 250 ? `opacity-0 scale-60` : `opacity-50 scale-90`}` : ``}
+            `}>
+                <NavLink className={`bg-[linear-gradient(90deg,#0A0B5F_0%,#242B8E_50%,#453F9A_100%)] flex gap-5 items-center py-3 md:py-4 px-5 md:px-7 border border-[#30363d] rounded-full mb-4 transition-all duration-150 active:duration-50 active:scale-97`}>
+                    <img 
+                        src="https://cdn-icons-png.flaticon.com/128/3196/3196017.png" 
+                        alt="buqalamun, hamilyon" 
+                        className="h-[37px] md:h-[45px]" 
+                    />
+                    <p className="text-[17px] md:text-[20px] tracking-[0.5px] font-semibold">Create your team on TeamFlow</p>
+                    <ChevronRight size={19} />
                 </NavLink>
-            </div>
 
+                <h2 className={`text-[47px] md:text-[54px] lg:text-[64px] lg:my-6 font-semibold text-center leading-[54px] md:leading-[64px]`}>Build real projects with your team.</h2>
+                <p className="text-[14px] md:text-[20px] text-[#cacaca] text-center my-4 leading-[18px] md:leading-[27px]">Collaborate with other developers, manage tasks, and <br /> turn ideas into real working products — all in one place.</p>
+
+                <div className="flex z-5 items-center gap-4 mb-14 md:flex-row flex-col">
+
+                    <div className={`bg-white flex items-center gap-1 p-1 rounded-[8px] h-[50px] sm:h-[55px]`}>
+                        <input type="text" placeholder="Enter your email" className="placeholder:text-[#6d6d6d] text-black text-[14px] sm:text-[16px] font-semibold outline-none h-full px-3 sm:px-5" />
+                        <button className="bg-[#1a7f37] text-white h-full flex items-center gap-1 rounded-[6px] px-6 font-semibold text-[14px] sm:text-[18px] cursor-pointer transition-all duration-150 active:duration-50 active:scale-95">
+                            Sign up for TeamFlow
+                        </button>
+                    </div>
+
+                    <NavLink to={'/'} className={`border-2 h-[55px] px-15 rounded-[8px] hidden md:flex items-center justify-center text-[18px] font-semibold cursor-pointer transition-all duration-150 active:duration-50 active:scale-95`}>
+                        Login
+                    </NavLink>
+                </div>
+            </div>
 
             <div className={`fixed bottom-0 right-0 w-full flex items-center justify-between`}>
                 <img 
@@ -72,7 +80,17 @@ export default function StartHomePage(){
 
         </div>
 
-        <p className="px-[32px]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique dicta hic illo, enim dolor deserunt aspernatur repudiandae quibusdam provident facere incidunt necessitatibus perferendis eligendi quaerat, mollitia culpa delectus ab, itaque deleniti cumque cum debitis nisi soluta quod. Cupiditate reiciendis, ab culpa quos ratione reprehenderit laudantium aliquid sit rerum, exercitationem ipsam sed natus? Quidem voluptas praesentium, illum natus blanditiis ab officiis perferendis optio animi minus velit a labore eligendi at. Deleniti iusto blanditiis accusamus aut quasi et ullam vitae officia debitis odit minus deserunt omnis incidunt, quae excepturi voluptatibus, earum fugit, tempora beatae neque nostrum ipsam reiciendis rerum. Iure nihil commodi eum ad distinctio eius esse deleniti, voluptas soluta assumenda adipisci sit quibusdam veritatis quo inventore iusto minima incidunt, non natus quis. Adipisci eveniet veniam quaerat saepe sed, suscipit fugit accusantium, nobis ratione nam consequuntur non libero similique dignissimos necessitatibus molestias at, ut id laudantium odio eos maxime ea impedit error. Animi, sunt reprehenderit iusto magni dicta voluptatibus. Autem sunt eum deserunt vitae a excepturi qui iusto quia saepe natus minima incidunt reprehenderit, aliquam vero cum iure voluptatem tempora ullam omnis expedita! Eius magnam illum adipisci minus fuga molestias saepe temporibus eaque possimus? Praesentium architecto commodi dolore blanditiis dolorem. Aliquid harum eos natus illo commodi unde ut accusantium facilis autem culpa, nostrum nulla quos, quaerat, quis quasi esse minima qui. Debitis ipsam laborum fuga itaque consequatur, consectetur unde, iure, eveniet ullam repellat veritatis et iusto! Neque minus totam dicta perspiciatis, est excepturi inventore veniam amet corrupti id eos quod nobis dolore mollitia rerum laboriosam quia alias autem molestias, hic voluptatibus. Ea explicabo veniam, dolorum aut consequuntur sit deleniti eaque corrupti recusandae dolorem soluta perspiciatis velit, reprehenderit quasi rerum laboriosam, deserunt temporibus doloremque! Quaerat animi perferendis magnam eum voluptate reiciendis expedita voluptates aperiam. Quibusdam commodi natus vel sint rem officiis sunt autem delectus porro quisquam veniam voluptatem, earum iure beatae cumque quaerat nobis hic ipsa quis iste asperiores dolores. Perferendis voluptatibus atque, hic, accusantium est delectus, laborum veritatis eveniet officiis necessitatibus nisi fugiat deleniti fuga placeat sequi qui aperiam quam quod. Aliquid explicabo fuga qui dolorum? Culpa facilis nisi, possimus repellat sequi consectetur tenetur mollitia omnis, delectus rem doloremque alias ab, architecto nihil beatae eaque excepturi minus officia. Excepturi soluta reprehenderit, aperiam blanditiis libero dolorum optio, voluptate quia ut asperiores illum quo deserunt ratione quibusdam. Tenetur vitae doloribus sint officia explicabo, reiciendis suscipit quibusdam, aliquid blanditiis quia vel odit maxime cum corrupti rerum velit odio maiores consequuntur eos soluta dolores illo possimus saepe. Suscipit iusto enim, atque, incidunt pariatur exercitationem totam nihil officiis praesentium obcaecati eaque rerum et laborum ducimus unde hic non dicta eos voluptatum accusamus. Tempora quod expedita perspiciatis in a fugit, consequatur distinctio iste maiores aliquam, est impedit. Maxime eaque neque placeat numquam dolore laborum quibusdam possimus nostrum porro, quisquam rem iusto accusantium cumque dignissimos itaque magni, qui tempora expedita nihil repudiandae delectus. Natus aspernatur laudantium, recusandae tempore beatae necessitatibus quaerat. Doloribus minus unde vero ratione! Voluptatum nam nesciunt sunt culpa, accusamus est labore quae nulla dolor voluptatem architecto repellendus vero impedit assumenda velit necessitatibus cumque, perspiciatis, debitis non sequi officiis! Est, ratione? Aliquid beatae, quaerat non aliquam velit laudantium dolorem ratione nemo dolore magni deleniti sapiente mollitia delectus rerum minus labore saepe neque reiciendis itaque qui minima pariatur? Corrupti ipsam numquam delectus ducimus expedita nostrum saepe laboriosam tempore ratione? Corporis quas debitis ullam aliquid sapiente ratione atque rem, recusandae aut eaque tenetur iste nostrum dolore sed fugit consequuntur maiores numquam explicabo voluptate amet pariatur ex beatae. Consequuntur dolorem illo magni perferendis beatae eveniet cum accusamus exercitationem. Aspernatur, ipsum porro praesentium, laborum iste culpa vero repudiandae similique, saepe sequi soluta accusamus optio. Asperiores non expedita ullam nemo error? Explicabo sunt inventore alias ab, ad aliquam rerum voluptates, est magni ea cum quidem. Enim ipsa eaque sint vel blanditiis, deserunt delectus laboriosam officia quas consequatur praesentium repellat sequi perferendis nulla possimus facilis unde dolores distinctio, optio qui? Veritatis blanditiis repudiandae sint sed eligendi fuga minus voluptate ab hic? Assumenda officiis, reiciendis debitis porro voluptate odit totam. Doloribus nihil maxime voluptatem beatae. Enim hic eius distinctio voluptatem earum atque minima qui, culpa minus. Quae temporibus modi reiciendis quo et eius id sequi quisquam deleniti nemo dolorum, nam soluta at iusto voluptatibus, adipisci rem ex beatae sapiente officiis. Quasi placeat praesentium quisquam minima ipsa vitae architecto culpa iure? Labore earum harum ipsam assumenda ad cum dolore voluptates vel repellendus repellat modi, ratione doloremque veritatis provident porro at dolor et officia tenetur hic blanditiis quia, sunt quas! Excepturi, voluptates laborum ad ipsam necessitatibus illum, sed atque minima nihil, eveniet ipsum non nostrum veniam ex neque iste quis debitis voluptate quae nesciunt doloribus odit magnam quidem? Similique beatae ex laboriosam in velit, architecto ab perspiciatis repellat aspernatur suscipit iusto reiciendis officiis? Laboriosam, earum tenetur. Illum ducimus optio enim doloremque quibusdam! Ea distinctio facere similique ut explicabo corporis, necessitatibus excepturi. Optio aspernatur labore consequuntur beatae quasi, molestiae culpa maxime non libero, earum facere, illum magnam dolorem tempora harum ipsa et voluptatum. Ea ipsum, culpa perspiciatis eligendi cumque pariatur dicta eos quibusdam aut quo repudiandae provident quasi odio eaque corporis sapiente voluptate amet accusantium praesentium. Asperiores ipsa impedit libero autem quibusdam fugiat modi deserunt quisquam molestiae et? Autem nobis repellat vel officia quis ducimus dolores, ipsam error sequi, voluptate quo! Voluptate itaque inventore nam sapiente doloribus aspernatur nobis aliquid quaerat consequatur quos, sequi illum, ratione soluta et dolorum exercitationem ex eius quis atque sunt nesciunt eveniet aperiam at. Ab aliquam nostrum unde alias magni modi voluptate ad non placeat maxime nam quasi quaerat illo nemo molestias quisquam, consectetur optio. Voluptate, modi magnam, nulla fuga ducimus placeat quaerat assumenda unde eos ad maxime deserunt consequatur optio a ullam sint quo harum eveniet maiores temporibus repudiandae delectus fugit minus? Ipsa aut quae quasi, alias culpa minus minima expedita, cumque exercitationem nobis quia unde sequi iure commodi deleniti voluptates vero delectus temporibus est nulla mollitia? Temporibus odit, reprehenderit explicabo repellat consectetur alias blanditiis. Quas ex incidunt non. Ex quo corrupti facilis natus, temporibus id voluptate nemo tempore aliquam deserunt?</p>
+        <div className={`relative w-full border min-h-[500px] px-[32px] max-w-[1100px] mx-auto`}>
+            <div>
+                <video 
+                    src={video} 
+                    autoPlay 
+                    loop 
+                    muted
+                    playsInline
+                ></video>
+            </div>
+        </div>
         
     </div>
 }
--- src/shared/components/StartHeader.jsx
+++ src/shared/components/StartHeader.jsx
@@ -1,8 +1,10 @@
-import { ChevronDown, Search, Workflow } from "lucide-react";
-import { useEffect, useState } from "react"
+import { ChevronDown, ChevronRight, Code, FileText, LayoutTemplate, Menu, Search, Wallet, Workflow } from "lucide-react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import logoPNG from '../../../public/logos/teamflow-light.png'
 
 export default function StartHeader(){
+    const [hideMenu, setHideMenu] = useState(false)
     
     // Ozgina scroll bo'lganda Header orqa rangini o'zgartirishi uchun
     const [scrollY, setScrollY] = useState(0)
@@ -15,13 +17,14 @@ export default function StartHeader(){
 
     return <div className={`${scrollY > 0 ? `bg-[#0d1117] shadow-lg` : ``} w-full h-[72px] flex items-center sticky top-0 left-0 z-10 transition-all duration-200 selection:bg-white/0`}>
         <div className="px-[32px] mx-auto w-full flex items-center justify-between h-full">
+
             <div className="h-full flex items-center gap-8">
                 <div className="h-full flex items-center gap-2 cursor-pointer">
-                    <img src="../../../public/logos/teamflow-light.png" alt="" className="h-[40px]" />
+                    <img src={logoPNG} alt="" className="h-[40px]" />
                     <h1 className="font-[monospace] font-semibold text-[25px]">Team<span className="text-[#1db6b5]">Flow</span></h1>
                 </div>
 
-                <div className="h-full flex items-center gap-4 font-semibold">
+                <div className="h-full hidden lg:flex items-center gap-4 font-semibold">
                     <NavLink to={'/'} className={`flex items-center text-[16px] gap-1 hover:text-[#acadc9]`}>
                         <p>Platform</p> 
                     </NavLink>
@@ -39,15 +42,74 @@ export default function StartHeader(){
                 </div>
             </div>
 
-            <div className="h-full flex items-center gap-7">
-                <div className={`border border-[#a7b3c3] w-[340px] bg-[#262e47] shadow-lg rounded-[7px] flex items-center gap-2 py-[6px] px-2`}>
+            <div className="h-full flex items-center gap-4 lg:gap-7">
+                <div className={`border border-[#a7b3c3] xl:w-[340px] bg-[#262e47] shadow-lg rounded-[7px] hidden lg:flex items-center gap-2 py-[6px] px-2`}>
                     <Search color="#80819b" size={17} />
-                    <input type="text" placeholder="Search something..." className="flex-1 h-full text-[14px] outline-none placeholder:text-[#ccc] placeholder:font-light placeholder:tracking-[0.5px] placeholder:text-[14px]" />
-                    <Workflow color="#80819b" size={17} />
+                    <input type="text" placeholder="Search something..." className="xl:flex hidden flex-1 h-full text-[14px] outline-none placeholder:text-[#ccc] placeholder:font-light placeholder:tracking-[0.5px] placeholder:text-[14px]" />
+                    <Workflow color="#80819b" size={17} className="xl:flex hidden" />
                 </div>
 
-                <NavLink to={'/login'} className={`text-[14px] hover:text-[#a7b3c3] transition-all duration-200`}>Login</NavLink>
-                <NavLink to={'/signup'} className={`border py-[6px] px-4 rounded-[7px] text-[14px] hover:border-[#a7b3c3] hover:text-[#a7b3c3] transition-all duration-200`} >Sign Up</NavLink>
+                <NavLink to={'/login'} className={`text-[14px] hover:text-[#a7b3c3] transition-all duration-200 border lg:border-none py-[5px] px-4 lg:p-0 rounded-[7px]`}>Login</NavLink>
+                <NavLink to={'/signup'} className={`lg:flex hidden border py-[6px] px-4 rounded-[7px] text-[14px] hover:border-[#a7b3c3] hover:text-[#a7b3c3] transition-all duration-200`} >Sign Up</NavLink>
+                <button onClick={() => setHideMenu(true)} className="lg:hidden flex outline-none transition-all duration-150 active:duration-50 active:scale-95 cursor-pointer">
+                    <Menu />
+                </button>
+            </div>
+
+            {/* Hide Menu */}
+            <div 
+                onClick={() => setHideMenu(false)} 
+                className={`
+                    ${hideMenu ? 'translate-x-0' : 'translate-x-[100%]'} 
+                    fixed flex top-0 right-0 lg:hidden w-full sm:w-[300px] h-screen flex justify-end bg-[#0d1117]/20 backdrop-blur-[2px] transition-all duration-500
+                `}>
+                <div onClick={(e) => e.stopPropagation()} className={`w-full bg-[#0d1117] h-full transition-all duration-200`}>
+                    <div className="w-full h-[50px] py-1 flex relative items-center justify-center border-b border-zinc-800">
+                        <button onClick={() => setHideMenu(false)} className="absolute left-0 top-0 h-full px-3 text-[#1db6b5] transition-all duration-150 active:duration-50 active:scale-95 cursor-pointer">
+                            <ChevronRight size={30} />
+                        </button>
+                        <img src={logoPNG} alt="" className="h-[33px]" />
+                    </div>
+
+                    <div className="flex flex-col gap-2 px-4 pt-4">
+
+                        <div className={`border border-[#a7b3c3] xl:w-[340px] bg-[#262e47] shadow-lg rounded-[7px] flex items-center justify-between gap-2 py-[6px] px-2 mb-3`}>
+                            <Workflow color="#80819b" size={17} />
+                            <input type="text" placeholder="Search something..." className="flex-1 h-full text-[14px] outline-none placeholder:text-[#ccc] placeholder:font-light placeholder:tracking-[0.5px] placeholder:text-[14px]" />
+                            <Search color="#80819b" size={17} />
+                        </div>
+
+                        <NavLink 
+                            to={`/`}
+                            className={`border border-zinc-700 flex items-center justify-center sm:justify-start gap-2 py-2 px-4 rounded-lg cursor-pointer transition-all duration-150 active:duration-50 active:scale-95 bg-white/5`}>
+                            <LayoutTemplate size={17} />
+                            Platform
+                        </NavLink>
+                        <NavLink 
+                            to={`/`}
+                            className={`border border-zinc-700 flex items-center justify-center sm:justify-start gap-2 py-2 px-4 rounded-lg cursor-pointer transition-all duration-150 active:duration-50 active:scale-95 bg-white/5`}>
+                            <Wallet size={17} />
+                            Pricing
+                        </NavLink>
+                        <NavLink 
+                            to={`/`}
+                            className={`border border-zinc-700 flex items-center justify-center sm:justify-start gap-2 py-2 px-4 rounded-lg cursor-pointer transition-all duration-150 active:duration-50 active:scale-95 bg-white/5`}>
+                            <FileText size={17} />
+                            Documentation
+                        </NavLink>
+                        <NavLink 
+                            to={`/`}
+                            className={`border border-zinc-700 flex items-center justify-center sm:justify-start gap-2 py-2 px-4 rounded-lg cursor-pointer transition-all duration-150 active:duration-50 active:scale-95 bg-white/5`}>
+                            <Code size={17} />
+                            Open Source
+                        </NavLink>
+                        <NavLink 
+                            to={`/`}
+                            className={`bg-[#238636] w-full py-2 font-semibold flex items-center justify-center rounded-lg mt-2 transition-all duration-150 active:duration-50 active:scale-95`}>
+                            Sign Up
+                        </NavLink>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
